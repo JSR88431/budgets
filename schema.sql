ad13f4cdeb3206cc237328ed105ce9f0d99562c3
Justin Kooner <kooner92@gmail.com>
2018-10-09 15:07:59 -0700

    style homepage

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS budget;
+DROP DATABASE IF EXISTS budgetnew;
 -- Creates the "todolist" database --
-CREATE DATABASE budget;
+CREATE DATABASE budgetnew;
